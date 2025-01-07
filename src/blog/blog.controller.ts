53c38c8e469cec6f51e0b610e1912cc2ceba6ac3
Chethana Dilhari <chethanadilhari99@gmail.com>
2025-01-08 02:08:29 +0530

    protect endpoint

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete,Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,Req, UseGuards } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 @Controller('blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Req() req: any, @Body() createBlogDto: CreateBlogDto) {
     const userId = req.session.userId;
-    return this.blogService.create(userId,createBlogDto);
+    return this.blogService.create(userId, createBlogDto);
   }
 
   @Get()
@@ -19,16 +21,19 @@ export class BlogController {
     return this.blogService.findAll(query);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.blogService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto) {
     return this.blogService.update(+id, updateBlogDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.blogService.remove(+id);
